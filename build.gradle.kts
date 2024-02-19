a510998f1c87b79b96ef37d55e875431a3ee20ff
SoSeDiK <mrsosedik@gmail.com>
2024-02-19 03:01:10 +0200

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@544a2b6 copy spawner data exactly when silk touching
    PurpurMC/Purpur@c9d4c1c properly increment history index, closes #1472
    PurpurMC/Purpur@36974ea Updated Upstream (Paper)
    PurpurMC/Purpur@066e9f1 Updated Upstream (Paper)
    PurpurMC/Purpur@de54530 Updated Upstream (Paper)
    PurpurMC/Purpur@e4d30e0 Updated Upstream (Paper)
    PurpurMC/Purpur@486515b Updated Upstream (Pufferfish)
    PurpurMC/Purpur@3d52f06 Updated Upstream (Paper)
    PurpurMC/Purpur@236b06a Updated Upstream (Paper & Pufferfish)
    PurpurMC/Purpur@11391a2 [ci-skip] Updated Upstream (Paper)
    PurpurMC/Purpur@8ac38d9 Updated Upstream (Paper)
    PurpurMC/Purpur@6aebd42 Updated Upstream (Paper)
    PurpurMC/Purpur@eab2140 Add config to only lobotomize villagers that have been traded with (#1466)
    PurpurMC/Purpur@ce7eaa1 Updated Upstream (Paper)
    PurpurMC/Purpur@030cb9e Updated Upstream (Paper & Pufferfish)
    PurpurMC/Purpur@03a732c Updated Upstream (Paper)
    PurpurMC/Purpur@c9016fd properly implement ProviderSource for our spark jar provider
    PurpurMC/Purpur@15d3f29 Updated Upstream (Paper)
    PurpurMC/Purpur@00d3fac Disable extra entities trampling turtle eggs by default (#1484)
    PurpurMC/Purpur@17c5a77 Updated Upstream (Paper)
    PurpurMC/Purpur@425889d Updated Upstream (Paper)
    PurpurMC/Purpur@70a76c4 Updated Upstream (Paper)
    PurpurMC/Purpur@770fbe6 clean up damage type code for stonecutter and scissor patches
    PurpurMC/Purpur@d0e4b7b Updated Upstream (Paper)
    PurpurMC/Purpur@d66bc35 Updated Upstream (Paper)
    PurpurMC/Purpur@bcd90f7 Updated Upstream (Paper)
    PurpurMC/Purpur@5b468f2 Fix bee count payload lag/crash exploit and make it configurable. (#1491)
    PurpurMC/Purpur@8f371ac add an option to register "unused" minecraft commands

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,6 +86,13 @@ paperweight {
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Kiterino-Server"))
         }
+
+        patchTasks.register("generatedApi") {
+            isBareDirectory = true
+            upstreamDirPath = "paper-api-generator/generated"
+            patchDir = layout.projectDirectory.dir("patches/generated-api")
+            outputDir = layout.projectDirectory.dir("paper-api-generator/generated")
+        }
     }
 }
 
