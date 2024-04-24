15e6c68240a99eb354ef854546e401c8cf3eef3f
SoSeDiK <mrsosedik@gmail.com>
2024-04-25 02:31:10 +0300

    Updated Upstream (Purpur)
    Upstream has released updates that appear to apply and compile correctly
    
    Purpur Changes:
    PurpurMC/Purpur@69b25cc Updated Upstream (Paper)
    PurpurMC/Purpur@df23ed3 fix black text in GUI on gnome desktops
    PurpurMC/Purpur@943f9af Updated Upstream (Paper)
    PurpurMC/Purpur@5bf0962 Updated Upstream (Paper)
    PurpurMC/Purpur@b7e8084 Updated Upstream (Paper)
    PurpurMC/Purpur@2ebde54 Updated Upstream (Paper)
    PurpurMC/Purpur@48939f2 Add SIMD Support for Java 21 (#1499)
    PurpurMC/Purpur@a587420 Updated Upstream (Paper)
    PurpurMC/Purpur@bd0d44d Updated Upstream (Paper)
    PurpurMC/Purpur@b64f4f5 Updated Upstream (Paper)
    PurpurMC/Purpur@5d2d4e4 update gradle paperweight version
    PurpurMC/Purpur@d51b4a3 [ci-skip] worry about this later
    PurpurMC/Purpur@9069ede Add Configurable-villager-search-radius (#1478)
    PurpurMC/Purpur@0b95a80 Updated Upstream (Paper)
    PurpurMC/Purpur@f2a5ca8 Updated Upstream (Pufferfish)
    PurpurMC/Purpur@7377ee9 Updated Upstream (Paper)
    PurpurMC/Purpur@aca8b3a Updated Upstream (Paper)
    PurpurMC/Purpur@6b1ee98 Final 1.20.4 Paper Upstream \o/

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.11"
+    id("io.papermc.paperweight.patcher") version "1.5.14"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -17,8 +17,8 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
-    decompiler("net.minecraftforge:forgeflower:2.0.627.2")
+    remapper("net.fabricmc:tiny-remapper:0.10.1:fat")
+    decompiler("org.vineflower:vineflower:1.10.1")
     paperclip("io.papermc:paperclip:3.0.3")
 }
 
