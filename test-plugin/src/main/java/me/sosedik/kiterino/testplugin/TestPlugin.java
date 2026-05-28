8fd172d54efb46a8e74300feca6cc2e8ca81cd7a
SoSeDiK <mrsosedik@gmail.com>
2026-05-28 12:41:58 +0300

    Fix mount event & rider handling during cross-dimension teleports Add translatable shutdown message bandaid

--- test-plugin/src/main/java/me/sosedik/kiterino/testplugin/TestPlugin.java
+++ test-plugin/src/main/java/me/sosedik/kiterino/testplugin/TestPlugin.java
@@ -1,20 +1,17 @@
 package me.sosedik.kiterino.testplugin;
 
-import me.sosedik.kiterino.event.entity.EntityLoadsProjectileEvent;
 import me.sosedik.kiterino.testplugin.data.TestMaterials;
 import me.sosedik.kiterino.testplugin.data.TestMobEffects;
 import me.sosedik.kiterino.testplugin.data.TestEntityTypes;
-import org.bukkit.Material;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.EquipmentSlot;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class TestPlugin extends JavaPlugin implements Listener {
@@ -37,44 +34,17 @@ public final class TestPlugin extends JavaPlugin implements Listener {
             event.setRespawnLocation(event.getPlayer().getLocation());
     }
 
-    @EventHandler(priority = EventPriority.LOW)
-    public void onBlockInteract(PlayerInteractEvent event) {
-        if (event.getHand() != EquipmentSlot.OFF_HAND) return;
-        if (event.getClickedBlock() == null) return;
-
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onEvent(PlayerChatEvent event) {
         Player player = event.getPlayer();
-        if (player.hasActiveItem()) return;
+        Pig pig = player.getWorld().spawn(player.getLocation(), Pig.class);
+        pig.setRider(player);
 
-        if (player.getInventory().getItemInMainHand().getType() == Material.BOW)
-            player.startUsingItem(EquipmentSlot.HAND);
-        else if (player.getInventory().getItemInOffHand().getType() == Material.BOW)
-            player.startUsingItem(EquipmentSlot.OFF_HAND);
-    }
-
-    @EventHandler(priority = EventPriority.LOW)
-    public void onAirInteract(PlayerInteractEvent event) {
-        if (event.useItemInHand() == Event.Result.ALLOW) return;
-        if (event.getClickedBlock() != null) return;
-        if (event.getHand() == null) return;
-
-        Player player = event.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() != Material.BOW) return;
-
-        event.setUseItemInHand(Event.Result.ALLOW);
-        player.startUsingItem(event.getHand());
-    }
-
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onLoad(EntityLoadsProjectileEvent event) {
-        if (!event.getProjectile().isEmpty()) return;
-
-        event.setProjectile(new ItemStack(Material.ARROW));
-    }
-
-    @EventHandler
-    public void onBow(EntityShootBowEvent event) {
-        if (!(event.getEntity() instanceof Player player)) return;
-        player.sendMessage("Shoot!");
+        getServer().getScheduler().runTaskLater(this, () -> {
+            World first = Bukkit.getWorlds().getFirst();
+            World world = player.getWorld().equals(first) ? Bukkit.getWorlds().get(1) : first;
+            pig.teleportAsync(world.getSpawnLocation());
+        }, 20L);
     }
 
 }
